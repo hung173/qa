07da2e1482c0768e3768175a3c9a51ee9f911ad0
hung <hung@hung.org>
2023-12-07 11:16:10 +0700

    update auditing aware

--- src/main/java/org/hung/demo/common/Constants.java
+++ src/main/java/org/hung/demo/common/Constants.java
@@ -7,7 +7,11 @@ public final class Constants {
     }
 
     public static final class Authority {
-        public static String ROLE_ADMIN = "ADMIN";
-        public static String ROLE_USER = "USER";
+        public static final String ROLE_SYSTEM = "SYSTEM";
+        public static final String ROLE_ADMIN = "ADMIN";
+        public static final String ROLE_USER = "USER";
+        public static final String ROLE_ANONYMOUS = "ANONYMOUS";
+        public static final String AUTHORITY_CLAIM = "authorities";
+        public static final String ATTRIBUTE_CONSTANT = "sub";
     }
 }
